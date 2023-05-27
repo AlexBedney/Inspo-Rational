75ab4f4bfe13a5c72532128da39aa16995c56f07
Alex Bedney <bedneya@my.lanecc.edu>
2023-05-27 15:52:50 -0700

    Copied the webpack and package files from lab5, any additional packages will have to be installed manually. Next shall be the page's initial ID elements for blank page.

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const webpack = require('webpack');
+const path = require('path');
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const copyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: {
+      weather: './src/js/index.js',
+    },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: '[name].bundle.js',
+      assetModuleFilename: "images/[name][ext]",
+      clean: true,
+    },
+    target: 'web',
+    devServer: { 
+      static: "./dist"
+    }, 
+    devtool: 'source-map', 
+    module: {
+      rules: [	
+        { 
+          test: /\.js$/i,
+          exclude: /(node_modules)/,
+          use: { 
+            loader: 'babel-loader', 
+            options: {
+            presets: ['@babel/preset-env']
+          }}
+        }, 
+        { 
+          test: /\.css$/i, 
+          use: [ 'style-loader', 'css-loader' ]		
+        },
+        { 
+            test: /.s[ac]ss$/i, 
+            use: [ 'style-loader', 'css-loader' , 'sass-loader']		
+        },
+        {  
+          test: /\.(svg|eot|ttf|woff|woff2)$/i,  
+          type: "asset/resource",
+        },
+        {
+          test: /\.(png|jpg|gif)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+    plugins: [
+      new htmlWebpackPlugin({
+        template: path.resolve(__dirname, "./src/index.html"),
+        chunks: ["inspo"],
+        inject: "body",
+        filename: "index.html",
+      }),
+      /* new copyPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, "src/images"),
+            to: path.resolve(__dirname, "dist/images"),
+          },
+        ],
+      }),
+      */
+    ],
+}
+  
