8bb5711bf8718e2821e7d71e78340ea1fd1e5b9d
AlexBedney <bedneya@my.lanecc.edu>
2023-05-30 12:20:07 -0700

    Github is NOW saying there were uncommited changes from main, which inspite of it lying before, there should be few issues. Gonna copy some of it's code lines to new branch from StaticHTML to continue work on the project.

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,32 @@
-class index {
-    
+class Index {
+    // constructor to initialize all global vars and methods
+    constructor() {
+
+        this.$qteBody = document.getElementById("quoteBody");
+
+        console.log("Running constructor");
+
+        this.GetStoicQuote();
+    }
+
+    GetStoicQuote() {
+        const requestOptions = {
+            method: 'GET',
+            mode: 'cors',
+            credentials: "same-origin",
+            headers: { 'Content-Type': 'application/json' },
+            referrerPolicy: "origin-when-cross-origin"
+        };
+        
+        // CHANGE URL TO BE QUOTE_URL FROM .ENV WHEN IT'S WORKING
+        fetch(`https://api.themotivate365.com/stoic-quote`)
+        .then(response => {
+            this.$qteBody.innerHTML = response.quote + " - " + response.author;
+        }).catch(error => {
+            this.$qteBody.innerHTML = "Error in fetching quote :(";
+        });
+    }
 }
+
+let indexInit;
+window.onload = () => { indexInit = new Index(); }
